fbef0514d742afd976f85b6eb40fd7475ac37aa9
Alexis Gauthiez <sa_spag@hotmail.fr>
2014-03-09 18:09:06 +0100

    Under construction v1

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,6 @@
 window.onload = function() {
-    $('.qr').popup();
+    $('.popup').popup();
+
+    var ctx = document.getElementById("skill").getContext("2d");
+    var chart = new Chart(ctx).Radar(skills, null);
 }
